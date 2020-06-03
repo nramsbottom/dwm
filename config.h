8c004567afb1b2a867c0d8fa197a691ea3362366
Neil Ramsbottom <nramsbottom@hotmail.com>
2020-06-03 09:06:29 +0200

    Added audio controls.
    * super + minus : decrease volume 10%
    * super + equal : increase volume 10%
    
    Based on modifications made my Luke Smith in his version of dwm (https://github.com/LukeSmithxyz/dwn)

--- config.h
+++ config.h
@@ -84,6 +84,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_minus,  spawn,          SHCMD("pamixer --allow-boost -d 10") },
+	{ MODKEY,                       XK_equal,  spawn,          SHCMD("pamixer --allow-boost -i 10") },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
